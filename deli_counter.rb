eed78de9596cae64e4270fa08bb4b4ecf07c86c4
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 06:08:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 #The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
 def take_a_number(katz_deli, new)
   if(katz_deli.length == 0)
-    puts "Welcome, #{new}. You are number 1 in line.""
+    puts "Welcome, #{new}. You are number 1 in line."
     
   end
 end
