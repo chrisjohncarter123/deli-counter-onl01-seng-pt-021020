1cd7ce8c30f02bb398017b88fff637072255ad55
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 05:59:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,8 @@ end
 #The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
 def take_a_number(katz_deli, new)
   result = "The line is currently:"
-  katz_deli.each_with_index |name, index|
+  katz_deli.each_with_index do |name, index|
+    
+  end
 end
   
