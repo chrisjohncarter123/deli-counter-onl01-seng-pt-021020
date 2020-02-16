428d8267fb9b1d27fc2e3bf1986b1ce9dd31f6fd
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 06:04:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,18 +4,15 @@ katz_deli = []
 
 #"The line is currently: 1. Logan 2. Avi 3. Spencer"
 def line(other_deli)
-  result = ""
   if(other_deli.length == 0)
-    result = "The line is currently empty."
+    puts "The line is currently empty."
   else
     result = "The line is currently: "
     other_deli.each_with_index do |name, index|
       result += "#{index + 1}. #{name} "
     end
-  
+    puts result
   end
-  puts result
-  result
 end
 
 #The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
