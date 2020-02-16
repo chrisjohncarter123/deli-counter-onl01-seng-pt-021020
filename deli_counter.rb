4e0c9ae348fb26357c091b52f0d3d6b5d23ece01
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 05:54:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,14 @@ katz_deli = []
 def line(katz_deli)
   if(katz_deli.length == 0)
     puts "The line is currently empty."
+  else
+    puts
   end
 end
 
+#The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
 def take_a_number(katz_deli, new)
-  puts "#{new} + #{katz_deli.length}"
+  result = "The line is currently:"
+  katz_deli.each_with_index |name, index|
 end
   
