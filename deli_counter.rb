2f0240fd652dc94d596d9a550230db0135fe1d1a
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 05:56:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
     result = "The line is currently empty."
   else
     result = "The line is currently: "
-    katz_deli.each_with_index do |name, index|
+    katz_deli.each_with_index do |name index|
       result += "#{index}. #{name} "
     end
   end
